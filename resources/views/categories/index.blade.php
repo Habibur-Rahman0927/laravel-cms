42cb33afaca4be9531ab7584f6dd2aad9ac040cc
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-13 22:29:37 +0600

    categroy delete and curd done

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -23,11 +23,38 @@
                             </td>
                             <td>
                                 <a href="{{route('categories.edit', $category->id)}}" class="btn btn-info float-end">Edit</a>
+                                <button type="button" class="btn btn-danger float-end mx-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Delete
+                </button>
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
+                <!-- Modal -->
+                <form action="{{ route('categories.destroy', $category->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Delete Category</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p class="text-center text-bold">
+                            Are you sure want to delete this category?
+                        </p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Go, Back</button>
+                        <button type="submit" class="btn btn-danger">Yes, Delete</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                </form>
         </div>
     </div>
 @endsection
